e12e6f7684096c47c9e059f7cd8b17cc1db1a9f2
Phoenix-0001 <149677560+Phoenix-0001@users.noreply.github.com>
2024-07-31 16:42:22 +0530

    FIRST PUSH

---
+++ app/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+require("dotenv").config();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH,
+  projectId: process.env.NEXT_PUBLIC_PID,
+  storageBucket: process.env.NEXT_PUBLIC_SBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
