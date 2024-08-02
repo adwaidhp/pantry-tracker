862718053c452079888c2b2f3cc790595a6645e8
Phoenix-0001 <149677560+Phoenix-0001@users.noreply.github.com>
2024-08-02 20:24:17 +0530

    firebase update

--- app/firebase.js
+++ app/firebase.js
@@ -4,7 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
 require("dotenv").config();
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
